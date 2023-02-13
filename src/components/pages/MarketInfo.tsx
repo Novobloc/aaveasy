6057d0e98e8ddd6f0c390bbe6051b4b88baa847c
Sandeep Kumar <satyasandeep786@gmail.com>
2023-02-13 23:32:58 +0530

    feat: Integrate Market info api

--- src/components/pages/MarketInfo.tsx
+++ src/components/pages/MarketInfo.tsx
@@ -1,42 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { aaveMarketInfo } from "../../utils/functions";
 
-const payments = [
+const initialAssetList = [
   {
-    id: 1,
-    date: "1/1/2020",
-    datetime: "2020-01-01",
-    description: "Business Plan - Annual Billing",
-    amount: "CA$109.00",
-    href: "#"
+    underlyingAsset: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    symbol: "DAI",
+    isActive: true,
+    isFreezed: true,
+    borrowingEnabled: true,
+    stableBorrowRateEnabled: false,
+    variableBorrowRate: "0.01539236568068016961",
+    stableBorrowRate: "0.03962202772629728824",
+    liquidityRate: "0.00363689010342812663",
+    totalLiquidity: "4160898.627476978410366572",
+    lastUpdateTimestamp: 1676283659,
+    aTokenAddress: "0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d",
+    totalBorrows: "256423.850966812344867115",
+    id: "0x6b175474e89094c44da98b954eedeac495271d0f0x24a42fd28c976a61df5d00d0599c34c4f90748c8",
+    totalLiquidityUSD: "4175179.49387229818234328727",
+    totalBorrowsUSD: "257303.93843927521415157394",
+    interestPerSecond: "0.00417517949435380071"
   }
 ];
 
-const assetList = [
-  {
-    id: 1,
-    symbol: "USDC",
-    balance: "34.1233",
-    isBuySell: true,
-    isBorrow: true,
-    isSupply: true,
-    explorerLink: "",
-    logo: "https://assets.coingecko.com/coins/images/6319/thumb/USD_Coin_icon.png?1547042389",
-    APY: 2.3
-  },
-  {
-    id: 1,
-    symbol: "BTC",
-    balance: "1.67",
-    isBuySell: true,
-    isBorrow: false,
-    isSupply: true,
-    explorerLink: "",
-    logo: "https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579",
-    APY: 1.7
-  }
-];
+export default function MarketInfo() {
+  const [assetList, setAssetList] = useState(initialAssetList);
 
-export default function Example() {
+  useEffect(() => {
+    (async () => {
+      const data = await aaveMarketInfo();
+      console.log(data, "data");
+      setAssetList(data);
+    })();
+  }, []);
+
   return (
     <>
       <section aria-labelledby="billing-history-heading">
@@ -47,22 +44,25 @@ export default function Example() {
             </h2>
           </div>
           <div className="mt-6 flex flex-col">
-            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="-my-2  sm:-mx-6 lg:-mx-8">
               <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                 <div className="overflow-hidden border-t border-gray-200">
-                  <table className="min-w-full divide-y divide-gray-200">
+                  <table className="min-w-full border-separate" style={{ borderSpacing: 0 }}>
                     <thead className="bg-gray-50">
                       <tr>
-                        <th scope="col" className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                        <th scope="col" className="sticky top-0 px-6 py-3 text-left text-sm font-semibold text-gray-900">
                           Asset
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
-                          Balance
+                        <th scope="col" className="sticky top-0 px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                          isActive
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                        <th scope="col" className="sticky top-0 px-6 py-3 text-left text-sm font-semibold text-gray-900">
                           APY
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                        <th scope="col" className="sticky top-0 px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                          totalBorrowsUSD
+                        </th>
+                        <th scope="col" className="sticky top-0 px-6 py-3 text-left text-sm font-semibold text-gray-900">
                           Action
                         </th>
                         <th scope="col" className="relative px-6 py-3 text-left text-sm font-medium text-gray-500">
@@ -70,49 +70,46 @@ export default function Example() {
                         </th>
                       </tr>
                     </thead>
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                      {assetList.map((asset) => (
-                        <tr key={asset.id}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                            <div className="flex items-center">
-                              <div className="h-10 w-10 flex-shrink-0">
-                                <img className="h-10 w-10 rounded-full" src={asset.logo} alt="" />
+                    <tbody className="bg-white">
+                      {assetList
+                        // .filter((item) => item.borrowingEnabled === true)
+                        .map((asset: any) => (
+                          <tr key={asset.id}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                              <div className="flex items-center">
+                                <div className="h-10 w-10 flex-shrink-0">
+                                  <img className="h-10 w-10 rounded-full" src={asset.logo} alt="" />
+                                </div>
+                                <div className="ml-4">
+                                  <div className="font-medium text-gray-900">{asset.symbol}</div>
+                                </div>
                               </div>
-                              <div className="ml-4">
-                                <div className="font-medium text-gray-900">{asset.symbol}</div>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{asset.balance}</td>
-                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{asset.APY}%</td>
-                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-                            {asset.isBuySell && (
+                            </td>
+                            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{asset.isActive ? "YES" : "NO"}</td>
+                            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{Number(asset.interestPerSecond).toFixed(4)}%</td>
+                            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{Number(asset.totalBorrowsUSD).toFixed(2)} USD</td>
+                            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
                               <a href={asset.explorerLink} className="text-orange-600 hover:text-orange-900">
                                 Buy / Sell
                               </a>
-                            )}{" "}
-                            {""}
-                            {""}
-                            {asset.isSupply && (
+                              &nbsp; &nbsp; &nbsp; &nbsp;
                               <a href={asset.explorerLink} className="text-orange-600 hover:text-orange-900">
                                 Supply
                               </a>
-                            )}{" "}
-                            {""}
-                            {""}
-                            {asset.isBorrow && (
+                              &nbsp; &nbsp; &nbsp; &nbsp;
+                              {asset.borrowingEnabled && (
+                                <a href={asset.explorerLink} className="text-orange-600 hover:text-orange-900">
+                                  Borrow
+                                </a>
+                              )}
+                            </td>
+                            <td className="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
                               <a href={asset.explorerLink} className="text-orange-600 hover:text-orange-900">
-                                Borrow
+                                View
                               </a>
-                            )}
-                          </td>
-                          <td className="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
-                            <a href={asset.explorerLink} className="text-orange-600 hover:text-orange-900">
-                              View
-                            </a>
-                          </td>
-                        </tr>
-                      ))}
+                            </td>
+                          </tr>
+                        ))}
                     </tbody>
                   </table>
                 </div>
