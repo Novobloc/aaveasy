e079908fdfaaef2dbe8233c401363d08c3028310
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2023-02-13 21:07:21 +0530

    feat: Integrate Arcada

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,43 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { useSmartAccountContext } from "./context/SmartAccountContext/SmartAccountContext";
-import { useGlobalContext } from "./context/GlobalContext/GlobalContext";
+
+// import { useGlobalContext } from "./context/GlobalContext/GlobalContext";
 import Loader from "./layouts/Loader";
 import Home from "./components/Home";
 import AaveApp from "./components/AaveApp";
-// import { useWeb3AuthContext } from "./context/SocialLoginContext/SocialLoginContext";
 
 /** Layout */
 import Header from "./layouts/Header";
 
-const App = () => {
-  const { appLoading } = useGlobalContext();
+import { Auth, useAuth } from "@arcana/auth-react";
 
-  if (appLoading) return <Loader />;
-
-  return (
-    <div className="app-container app-theme-white flex flex-col w-full text-gray-700 bg-white">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/app" element={<AaveApp />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+const onLogin = () => {
+  // Route to authenticated page
 };
 
+function App() {
+  const auth = useAuth();
+  return (
+    <div>
+      {auth.loading ? (
+        <Loader />
+      ) : auth.isLoggedIn ? (
+        <div className="app-container app-theme-white flex flex-col w-full text-gray-700 bg-white">
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/app" element={<AaveApp />} />
+            </Routes>
+          </Router>
+        </div>
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"light"} onLogin={onLogin} />
+        </div>
+      )}
+    </div>
+  );
+}
+
 export default App;
-
-// import { Auth, useAuth } from "@arcana/auth-react";
-
-// function App() {
-//   const auth = useAuth();
-//   const onLogin = async () => {
-//     console.log("click");
-    
-//     console.log("connect" ,  auth.provider);
-//   };
-//   console.log("auth: ", auth);
-//   return (
-//     <div>
-//       <button onClick={onLogin}> Connect </button>
-//       {/* <Auth externalWallet={true} theme={"light"} /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// // {auth.loading ? (
-// //   "Loading"
-// // ) : auth.isLoggedIn ? (
-// //   <p>Logged In</p>
-// // ) : (
-// //   <div>
-// //     {/* <Auth externalWallet={true} theme={"light"} onLogin={onLogin} /> */}
-// //   </div>
-// // )}
--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,35 @@
 import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider, CHAIN } from "@arcana/auth";
-import { ethers } from "ethers";
+import { ProvideAuth } from "@arcana/auth-react";
 
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
 import "./index.css";
 
-// import { GlobalProvider } from "./context/GlobalContext/GlobalContext";
+import { GlobalProvider } from "./context/GlobalContext/GlobalContext";
 
 const appAddress = "f15f444552fd0b88c80f0373f7c4433b585a74da";
 
-const auth = new AuthProvider(`${appAddress}`, {
+const provider = new AuthProvider(`${appAddress}`, {
   chainConfig: {
     chainId: CHAIN.ETHEREUM_GOERLI, //defaults to CHAIN.ETHEREUM_MAINNET
     rpcUrl: "https://side-fabled-violet.ethereum-goerli.discover.quiknode.pro/16bf13dae688d98d4fed25a482585b3ede55c6e0/" //defaults to 'https://rpc.ankr.com/eth'
   }
 });
 
-const initialize = async () => {
-  try {
-    await auth.init();
-    console.log("asdf");
-
-    // const arcanaProvider = await auth.loginWithLink("shiva2nani.mangina@gmail.com");
-    // const provider = new ethers.providers.Web3Provider(arcanaProvider);
-
-    const blno = await auth.getUser();
-    console.log("blno: ", blno);
-    // 14983200
-  } catch (e) {
-    console.log("e: ", e);
-    // log error
-  }
-};
-
 const element = document.getElementById("root");
 const root = createRoot(element!);
 
 const Index = () => {
-  useEffect(() => {
-    initialize();
-  }, []);
-
   return (
     <React.StrictMode>
-      {/* <GlobalProvider> */}
-      <App />
-      {/* </GlobalProvider> */}
+      <ProvideAuth provider={provider}>
+        <GlobalProvider>
+          <App />
+        </GlobalProvider>
+      </ProvideAuth>
     </React.StrictMode>
   );
 };
