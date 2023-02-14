0937c7dec8ce00658a01a71421bee0558d03019a
Sandeep Kumar <satyasandeep786@gmail.com>
2023-02-14 22:04:30 +0530

    feat: Modify Net worth UI

--- src/components/pages/Balance.tsx
+++ src/components/pages/Balance.tsx
@@ -1,112 +1,51 @@
 import React from "react";
-
-const projects = [
+const transactions = [
   {
-    id: 1,
-    title: "GraphQL API",
-    initials: "GA",
-    team: "Engineering",
-    members: [
-      {
-        name: "Dries Vincent",
-        handle: "driesvincent",
-        imageUrl:
-          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      },
-      {
-        name: "Lindsay Walton",
-        handle: "lindsaywalton",
-        imageUrl:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      },
-      {
-        name: "Courtney Henry",
-        handle: "courtneyhenry",
-        imageUrl:
-          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      },
-      {
-        name: "Tom Cook",
-        handle: "tomcook",
-        imageUrl:
-          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      }
-    ],
-    totalMembers: 12,
-    lastUpdated: "March 17, 2020",
-    pinned: true,
-    bgColorClass: "bg-pink-600"
+    symbol: "DAI",
+    balance: 23.48,
+    apy: 1.23
+  },
+  {
+    symbol: "MATIC",
+    balance: 2.8,
+    apy: 0.3
   }
-  // More projects...
 ];
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Balance() {
   return (
-    <>
-      <div className="px-4 sm:px-6">
-        <h2 id="billing-history-heading" className="leading-6 text-xl font-semibold text-gray-900">
-          Portfolio
-        </h2>
-      </div>
-      <div className="mt-6 flex flex-col">
-        <div className="-my-2  sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <div className="overflow-hidden border-t border-gray-200">
-              <table className="min-w-full">
-                <thead>
-                  <tr className="border-t border-gray-200">
-                    <th className="border-b border-gray-200 bg-gray-50 px-6 py-3 text-left text-sm font-semibold text-gray-900" scope="col">
-                      <span className="lg:pl-2">Project</span>
+    <div>
+      <h2 className="leading-6 text-xl font-semibold text-gray-900  lg:-mx-2">Balance</h2>
+      <div className="mt-4 flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full align-middle ">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                      Symbol
                     </th>
-                    <th className="border-b border-gray-200 bg-gray-50 px-6 py-3 text-left text-sm font-semibold text-gray-900" scope="col">
-                      Members
+                    <th scope="col" className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Balance
                     </th>
-                    <th
-                      className="hidden border-b border-gray-200 bg-gray-50 px-6 py-3 text-right text-sm font-semibold text-gray-900 md:table-cell"
-                      scope="col">
-                      Last updated
+                    <th scope="col" className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      APY
                     </th>
-                    <th className="border-b border-gray-200 bg-gray-50 py-3 pr-6 text-right text-sm font-semibold text-gray-900" scope="col" />
+                    <th scope="col" className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6">
+                      <span className="sr-only">Edit</span>
+                    </th>
                   </tr>
                 </thead>
-                <tbody className="divide-y divide-gray-100 bg-white">
-                  {projects.map((project) => (
-                    <tr key={project.id}>
-                      <td className="w-full max-w-0 whitespace-nowrap px-6 py-3 text-sm font-medium text-gray-900">
-                        <div className="flex items-center space-x-3 lg:pl-2">
-                          <div className={classNames(project.bgColorClass, "flex-shrink-0 w-2.5 h-2.5 rounded-full")} aria-hidden="true" />
-                          <a href="#" className="truncate hover:text-gray-600">
-                            <span>
-                              {project.title} <span className="font-normal text-gray-500">in {project.team}</span>
-                            </span>
-                          </a>
-                        </div>
-                      </td>
-                      <td className="px-6 py-3 text-sm font-medium text-gray-500">
-                        <div className="flex items-center space-x-2">
-                          <div className="flex flex-shrink-0 -space-x-1">
-                            {project.members.map((member) => (
-                              <img
-                                key={member.handle}
-                                className="h-6 w-6 max-w-none rounded-full ring-2 ring-white"
-                                src={member.imageUrl}
-                                alt={member.name}
-                              />
-                            ))}
-                          </div>
-                          {project.totalMembers > project.members.length ? (
-                            <span className="flex-shrink-0 text-xs font-medium leading-5">+{project.totalMembers - project.members.length}</span>
-                          ) : null}
-                        </div>
-                      </td>
-                      <td className="hidden whitespace-nowrap px-6 py-3 text-right text-sm text-gray-500 md:table-cell">{project.lastUpdated}</td>
-                      <td className="whitespace-nowrap px-6 py-3 text-right text-sm font-medium">
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {transactions.map((transaction) => (
+                    <tr key={transaction.symbol}>
+                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">{transaction.symbol}</td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{transaction.balance}</td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{transaction.apy}%</td>
+                      <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                         <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                          Edit
+                          Edit<span className="sr-only"></span>
                         </a>
                       </td>
                     </tr>
@@ -117,6 +56,6 @@ export default function Balance() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/pages/NetWorth.tsx
+++ src/components/pages/NetWorth.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
+import { CurrencyDollarIcon, ArrowRightIcon, PaperAirplaneIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
 
 const project = { name: "Net worth", amount: "240.57", href: "#", currency: "USD", bgColor: "bg-pink-600", icon: CurrencyDollarIcon };
 
 export default function Example() {
   return (
     <div>
-      <h2 className="text-sm font-medium text-gray-500">Pinned Projects</h2>
+      <h2 className="leading-6 text-xl font-semibold text-gray-900 lg:-mx-2">Net Worth</h2>
       <ul role="list" className="mt-3 ">
         <li className="col-span-1 flex rounded-md shadow-sm">
           <CurrencyDollarIcon className="h-16 w-16 " aria-hidden="true" />
@@ -19,15 +19,29 @@ export default function Example() {
                 {project.amount} {project.currency}
               </p>
             </div>
-            {/* <div className="flex-shrink-0 pr-2">
+            <div className="flex-shrink-0 pr-2">
               <button
                 type="button"
                 className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white bg-transparent text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span className="sr-only">Open options</span>
-                <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                <ArrowRightIcon className="h-5 w-5" aria-hidden="true" />
               </button>
-            </div> */}
+            </div>
           </div>
+        </li>
+        <li className="col-span-1 flex rounded-md shadow-sm mt-5">
+          <button
+            type="button"
+            className="mr-7 ml-7 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <PaperAirplaneIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+            Buy Crypto
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <ArrowUpIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+            Sell Crypto
+          </button>
         </li>
       </ul>
     </div>
